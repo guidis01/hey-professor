f812c0e2709c984ef551d7f6f121639eae37ffbe
guilherme silva <guilherme.augusto@globalhitss.com.br>
2023-12-06 10:35:48 -0300

    hey-17: applying frontend to our edit/update process

--- resources/views/question/index.blade.php
+++ resources/views/question/index.blade.php
@@ -44,6 +44,10 @@
                                     Publicar
                                 </button>
                             </x-form>
+
+                            <a href="{{ route('question.edit', $question) }}" class="hover:underline text-blue-500">
+                                Editar
+                            </a>
                         </x-table.td>
                     </x-table.tr>
                 @endforeach
